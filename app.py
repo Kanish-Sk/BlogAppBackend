0a4224579d88224a1ca8fb20ae84c75cb372d49d
Kanish <111266024+Kanish-Sk@users.noreply.github.com>
2023-08-14 13:31:04 +0530

    back-end files for deployment

---
+++ app.py
@@ -0,0 +1,87 @@
+from bson import ObjectId
+from flask import Flask, Response, request
+import json, os
+from pymongo import MongoClient
+from flask_cors import CORS
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def create_app():
+
+    app = Flask(__name__)
+    CORS(app)
+
+    mongo = MongoClient(os.getenv("MONGODB_URI"))
+    db = mongo["react_blog"]
+
+
+    ################################ home page #####################################
+
+    @app.route('/home', methods=["GET"])
+    def home():
+        blogs_details = list(db.blogs.find({}))
+        for blog in blogs_details:
+            blog["_id"] = str(blog["_id"])
+        return Response(
+            response=json.dumps(blogs_details, default=str),
+            status=200,
+            mimetype="application/json"
+        )
+
+    ################################ create blog page #####################################
+
+    @app.route('/createblog', methods=["POST"])
+    def createblog():
+        blogs = db["blogs"]
+        if request.json:
+            data = request.json
+            print(data)
+            blogs.insert_one(data)
+            
+        return Response(
+            response=True,
+            status=200,
+            mimetype="application/json"
+        )
+        
+    ################################ diplay each blog page #####################################
+
+    @app.route('/blogs/<string:blog_id>', methods=["GET"])
+    def get_blog(blog_id):
+        blog = db["blogs"].find_one({"_id": ObjectId(blog_id)})
+        blog["_id"] = str(blog["_id"])
+
+        return Response(
+            response=json.dumps(blog),
+            status=200,
+            mimetype="application/json"
+        )
+
+    ################################ delte blog #####################################
+
+    @app.route('/blogs/<string:blog_id>', methods=["DELETE"])
+    def delete_blog(blog_id):
+        try:
+            result = db["blogs"].delete_one({"_id": ObjectId(blog_id)})
+            if result.deleted_count > 0:
+                return Response(
+                response="Blog deleted",
+                status=200,
+                mimetype="application/json"
+            )
+            else:
+                return Response(
+                response="Blog not found",
+                status=500,
+                mimetype="application/json"
+            )
+        except Exception as e:
+            return Response(
+                response="Can't delete blog",
+                status=500,
+                mimetype="application/json"
+            )
+    
+    return app
